bca1f2e8b179da0294a03c2342482f4d87d22ff8
Adelin Sofian <adelinmihai071@gmail.com>
2025-02-22 20:10:56 +0200

    echo builtin

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ int main() {
     if(input.find("echo") != std::string::npos) {
       std::string token = input.substr(5);
       std::cout << token << std::endl;
+    } else if(input.find("exit") != std::string::npos) {
+      return input.substr(5).empty() ? 0 : std::stoi(input.substr(5));
     } else {
       std::cout << input << ": command not found" << std::endl;
     }
