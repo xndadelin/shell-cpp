88987f480402e2f11cac39b5f4b9a8c052ceb625
Adelin Sofian <adelinmihai071@gmail.com>
2025-02-22 19:32:54 +0200

    deleted comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
