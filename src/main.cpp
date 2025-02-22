8857934a8552ae18663bfc2967449014f21ad22a
Adelin Sofian <adelinmihai071@gmail.com>
2025-02-22 19:34:12 +0200

    deleted comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 int main() {
+  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  // Uncomment this block to pass the first stage
+  std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
